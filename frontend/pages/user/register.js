bc08ab29949d4a6b1b0dc71447a828ed5ba14007
Karl Behrens <karlbehrensg@gmail.com>
2020-09-07 01:07:46 -0300

    Tarjeta de registro

--- frontend/pages/user/register.js
+++ frontend/pages/user/register.js
@@ -2,13 +2,48 @@ import Link from 'next/link'
 
 export default function Register() {
   return (
-    <>
-      <h1>Pagina de registro</h1>
-      <h5>
-        <Link href='/'>
-          <a>Ir al home</a>
-        </Link>
-      </h5>
-    </>
+    <div className="container col-md-8 align-items-center">
+      <div className="card">
+        <div className="card-body">
+          <form>
+            <div className="form-row">
+              <div className="form-group col-md-4">
+                <label>Nombre</label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="form-group col-md-4">
+                <label>Apellido Paterno</label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="form-group col-md-4">
+                <label>Apellido Materno</label>
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+            <div className="form-row">
+              <div className="form-group col-md-6">
+                <label htmlFor="inputEmail4">Email</label>
+                <input type="email" className="form-control" />
+              </div>
+              <div className="form-group col-md-6">
+                <label htmlFor="inputEmail4">Confirmar email</label>
+                <input type="email" className="form-control" />
+              </div>
+            </div>
+            <div className="form-row">
+              <div className="form-group col-md-6">
+                <label htmlFor="inputPassword4">Contraseña</label>
+                <input type="password" className="form-control" />
+              </div>
+              <div className="form-group col-md-6">
+                <label htmlFor="inputPassword4">Confirmar contraseña</label>
+                <input type="password" className="form-control" />
+              </div>
+            </div>
+            <button type="submit" className="btn btn-primary">Registrate</button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
