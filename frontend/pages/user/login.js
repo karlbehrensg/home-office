b7f898ccf7432c41979f9ac169da8693589cb571
Karl Behrens <karlbehrensg@gmail.com>
2020-09-07 00:58:47 -0300

    Tarjeta de iniciar sesion

--- frontend/pages/user/login.js
+++ frontend/pages/user/login.js
@@ -2,13 +2,22 @@ import Link from 'next/link'
 
 export default function Login() {
   return (
-    <>
-      <h1>Iniciar sesion</h1>
-      <h5>
-        <Link href='/'>
-          <a>Ir al home</a>
-        </Link>
-      </h5>
-    </>
+    <div className="container col-md-4 align-items-center">
+      <div className="card">
+        <div className="card-body">
+          <form>
+            <div className="form-group">
+              <label htmlFor="exampleInputEmail1">Email</label>
+              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="exampleInputPassword1">Contraseña</label>
+              <input type="password" className="form-control" id="exampleInputPassword1" />
+            </div>
+            <button type="submit" className="btn btn-primary">Iniciar Sesion</button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
