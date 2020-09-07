d7230eb7d0eb3365e63d668c7b1a690a84f1cee5
Karl Behrens <karlbehrensg@gmail.com>
2020-09-06 22:32:26 -0300

    Routing de home y registro

---
+++ frontend/pages/user/register.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function Register() {
+  return (
+    <>
+      <h1>Pagina de registro</h1>
+      <h5>
+        <Link href='/'>
+          <a>Ir al home</a>
+        </Link>
+      </h5>
+    </>
+  )
+}
