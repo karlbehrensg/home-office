76749d5bae6b63db63d5a0f52d9eb4722b0c420f
Karl Behrens <karlbehrensg@gmail.com>
2020-09-30 20:25:17 -0300

    Implementacion de react hook form

--- frontend/pages/login/index.js
+++ frontend/pages/login/index.js
@@ -1,18 +1,51 @@
 import Link from 'next/link'
+import {useForm} from "react-hook-form";
 
 export default function Index() {
+
+  const {register, handleSubmit, errors} = useForm()
+
+  const onSubmit = (data) => {
+    console.log(JSON.stringify(data))
+  }
+
   return (
     <div className="container col-md-4 align-items-center">
       <div className="card">
         <div className="card-body">
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className="form-group">
               <label htmlFor="exampleInputEmail1">Email</label>
-              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+              <input
+                ref={register({required: true})}
+                name="email"
+                type="email"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+              {
+                errors?.email &&
+                <div className="alert alert-danger" role="alert">
+                  Ingrese un correo
+                </div>
+              }
             </div>
             <div className="form-group">
               <label htmlFor="exampleInputPassword1">Contraseña</label>
-              <input type="password" className="form-control" id="exampleInputPassword1" />
+              <input
+                ref={register({required: true})}
+                name="password"
+                type="password"
+                className="form-control"
+                id="exampleInputPassword1"
+              />
+              {
+                errors?.password &&
+                <div className="alert alert-danger" role="alert">
+                  Ingrese una contraseña
+                </div>
+              }
             </div>
             <button type="submit" className="btn btn-primary">Iniciar Sesion</button>
           </form>
